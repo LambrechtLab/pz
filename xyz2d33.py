8bf7c158c5b934247d1c534fe68b96030b0a53f1
Daniel Lambrecht <dslambrecht@gmail.com>
2017-09-21 18:06:03 -0400

    Finished xyz2d33

--- xyz2d33.py
+++ xyz2d33.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 
 # Calculates piezoelectric coefficient from taking the difference between two XYZ files.
-# Usage: xyz2d33.py xyz-file1 xyz-file2
 
 import sys
+import math
 
-if len(sys.argv) != 3:
-	print("Usage: xyz2d33.py xyz-file1 xyz-file2")
+if len(sys.argv) != 4:
+	### USAGE ###
+	print("Usage: xyz2d33.py xyz-file1 xyz-file2 Field1")
+	print("")
+	print("xyz-file1 is the geometry _after_ in the field.")
+	print("xyz-file2 is the geometry _without_ the field.")
+	print("Field1 is the field strength.")
+	print("Note: Field2 is assumed to always be zero.")
+	###
 	sys.exit(1)
 
 
@@ -18,6 +25,8 @@ print(f_xyz1)
 f_xyz2 = sys.argv[2]
 print(f_xyz2)
 
+F1 = float(sys.argv[3])
+F2 = 0
 
 def read_XYZ(filename):
 	f = open(filename, "r")
@@ -41,6 +50,34 @@ if NAtom1 != NAtom2:
 	print("NAtom1 = %d  NAtom2 = %d  => mismatch!" % (NAtom1, NAtom2))
 	sys.exit(1)
 
+def distance(iXYZ, jXYZ):
+	
+	x1 = float(iXYZ[1])
+	y1 = float(iXYZ[2])
+	z1 = float(iXYZ[3])
+	x2 = float(jXYZ[1])
+	y2 = float(jXYZ[2])
+	z2 = float(jXYZ[3])
+	R = math.sqrt( (x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2 )
+
+	return R
+
+deltaF = F1 - F2
+
+
+d33 = 0
+i = 0
+j = 0
 for iAtom in range(NAtom1):
 	for jAtom in range(iAtom+1, NAtom1):
-		
+		Rij_mol1 = distance(XYZ1[iAtom], XYZ1[jAtom])		
+		Rij_mol2 = distance(XYZ2[iAtom], XYZ2[jAtom])		
+		deltaR = Rij_mol1 - Rij_mol2
+		d = (100/Rij_mol2) * deltaR / deltaF # (pm/V)
+		if d > d33:
+			d33 = d
+			i = iAtom
+			j = jAtom
+		print("d(%d,%d) = %.2f  pm/V" % (iAtom, jAtom, d))
+
+print("D33 = %.2f  pm/V     (atom pair: %d - %d)" % (d33, i, j))
